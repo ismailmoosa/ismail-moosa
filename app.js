939d591580b2db661c337f77dcbdfbd45568da4f
ismailmoosa <ismail.moosa@digitalskillsacademy.me>
2017-10-09 17:35:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var myAPP = angular.module('myAPP', [ 'ngRoute' ]);
-
+//adding routing to the app with 'ng route'
 myAPP.config(['$routeProvider',
     function (
         $routeProvider
-    ) {
+    ) { // 4 links for the routing.this is for ease of use and to change at one place instead of soo many places
           $routeProvider.
               when('/home', {
                   templateUrl: 'partials/home.html',
@@ -27,13 +27,13 @@ myAPP.config(['$routeProvider',
     $scope.todoList = [
     {Name:'Assignment!',Descr:'10th October 2017', done:false}];
 
-
+//add function to list
     $scope.todoAdd = function() {
         $scope.todoList.push({Name:$scope.Name,Descr:$scope.Descr, done:false});
         $scope.Name = "";
         $scope.Descr = "";
     };
-
+//delete function from list
     $scope.remove = function() {
         var oldList = $scope.todoList;
         $scope.todoList = [];
